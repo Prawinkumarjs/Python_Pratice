d3842f91778ab2e51a6db3796e31ce2792dd5ef4
sethuraj-2003 <sethuraj2003@gmail.com>
2026-06-19 17:31:47 +0530

    sethu da

--- hello.py
+++ hello.py
@@ -1,32 +1,34 @@
-# print("hi da...")
-# a = 12
-# print(a)
-# a = int(input("Enter the number: "))
-# print(a)
-# b = input("name: ")
-# print(" The name is " + b)
+# # print("hi da...")
+# # a = 12
+# # print(a)
+# # a = int(input("Enter the number: "))
+# # print(a)
+# # b = input("name: ")
+# # print(" The name is " + b)
 
 
 
-# print(*range(10,0,-1),sep='*')
+# # print(*range(10,0,-1),sep='*')
 
-# print(*range(1,5),sep='*')
-# print(*range(1,4),sep='*')
-# print(*range(1,3),sep='*')
-# print(*range(1,2),sep='*')
+# # print(*range(1,5),sep='*')
+# # print(*range(1,4),sep='*')
+# # print(*range(1,3),sep='*')
+# # print(*range(1,2),sep='*')
 
-# for i in range(5,1,-1):
-#     print(*range(1,i),sep="*")
+# # for i in range(5,1,-1):
+# #     print(*range(1,i),sep="*")
 
-# no desirer variable for storing input we can use for
-# for i in range(int(input())):
-#     print(i)
+# # no desirer variable for storing input we can use for
+# # for i in range(int(input())):
+# #     print(i)
 
-# for i in range(int(input("enter the number: "))):
-#     print(i, sep="*")
+# # for i in range(int(input("enter the number: "))):
+# #     print(i, sep="*")
 
-# print(*range(1,8,2),end= "\t",sep="*")
+# # print(*range(1,8,2),end= "\t",sep="*")
 
-# print(*range(0,21,2),sep=" ")
+# # print(*range(0,21,2),sep=" ")
 
-print(*range(10,0,-1))
+# print(*range(10,0,-1))
+
+print("vanakam da mapla chennai la irunthu")
