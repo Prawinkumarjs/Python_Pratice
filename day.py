23ae2bd266bbe03bf078c450ad3773bf00bf24e6
Prawinkumarjs <prawink554@gmail.com>
2026-06-23 18:03:26 +0530

    new

--- day.py
+++ day.py
@@ -54,21 +54,45 @@
 
 
 
-# Compress a String. (Example : aaabbcccc , a3b2c4)
+# # Compress a String. (Example : aaabbcccc , a3b2c4)
 
 
-a = "aaabbbcccz"
-result = ''
+# a = "aaabbbcccz"
+# result = ''
 
-st1 = [0]*26
+# st1 = [0]*26
 
-for i in a.lower():
-    st1[ord(i) - 97] += 1
-    # print(i, ':', ord(i) -97)
+# for i in a.lower():
+#     st1[ord(i) - 97] += 1
+#     # print(i, ':', ord(i) -97)
+#     # print(st1)  # here is for how the frequency is counted
+# # print(st1)  
+
+# index = 0
+
+# for i in st1:
+#     if i:
+#         print(i)    # for check purpose frequency     
+#         result = result + chr(index+97) + str(i) # new result = old result + char(97) + its string contacted frequency
+#     index += 1  # this out the if for update and assign
+
+# print(result)
 
 
+# First Non Repeating character
+# a = "swiss"
+# here w & i is not repeating but w is first non repeating character
 
+# method 1 in-built function
 
+a = 'swiss'
+dup = ""
+
+for i in a:
+    if a.count(i) == 1:
+        dup = dup + i
+        break
+print(dup)
 
     
 
