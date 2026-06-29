7415502401a53dff8f3997e512e86a76b2c52f64
Prawinkumarjs <prawink554@gmail.com>
2026-06-29 17:59:55 +0530

    square pattern

---
+++ pattern.py
@@ -0,0 +1,14 @@
+# # square pattern
+# a = int(input("Enter the Number: "))
+# for i in range(a):
+#     for j in range(a):
+#         print("*" ,end=" ")
+#     print()
+
+
+
+# square pattern without 2 loops
+
+b = int(input("Enter num: "))
+for i in range(b):
+    print("* " *b)
