2b32f7f105b24af267906b9277fd21cca1069193
Prawinkumarjs <prawink554@gmail.com>
2026-06-30 17:45:05 +0530

    hollow square

--- pattern.py
+++ pattern.py
@@ -14,3 +14,20 @@
 #     print("* " *b)
 
 
+
+# hollow square
+
+a = int(input("Enter num: "))
+
+for i in range(a):
+    container = ""
+    for j in range(a):
+        if(j == 0 or j == a-1 or i == 0 or i == a-1):
+            container += '*_'
+           
+        else:
+            container += '  '
+           
+    print(container[:-1])
+
+
