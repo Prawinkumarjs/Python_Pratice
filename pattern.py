a54fa19e6bf595d1ca957a42b8dd9936f7f378e3
Prawinkumarjs <prawink554@gmail.com>
2026-07-01 17:47:45 +0530

    left triangle with container

--- pattern.py
+++ pattern.py
@@ -41,12 +41,23 @@
 #         print('* '+'  '*(row-2)+'*')
 
 
-# left triangle
+# # left triangle without container
 
-a = int(input('Enter num: '))
+# a = int(input('Enter num: '))
+# for i in range(a):
+#     for j in range(i+1):
+#         print("* ", end=" ")
+#     print()
+
+# left triangle with container
+
+a = int(input("Enter num: "))
+
 for i in range(a):
+    container = ""
     for j in range(i+1):
-        print("* ", end=" ")
-    print()
+        container += "*_"
+    print(container[:-1])
+
 
  
