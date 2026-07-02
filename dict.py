8d2bb77ebd2720f33eddbc14863179fc5c9cb22d
Prawinkumarjs <prawink554@gmail.com>
2026-07-02 17:59:24 +0530

    grpng

--- dict.py
+++ dict.py
@@ -10,15 +10,31 @@
 #         fre[i] = 1
 # print(fre)
 
-# sort string wrt frequency
+# # sort string wrt frequency
 
-word = "deiii vaaaddaaaa"
-freq = {}
+# word = "deiii vaaaddaaaa"
+# freq = {}
 
-for i in word:
-    if i in freq:
-        freq[i]+=1
+# for i in word:
+#     if i in freq:
+#         freq[i]+=1
+#     else:
+#         freq[i] = 1
+# print(freq)
+# print(sorted(freq,key=lambda x:freq[x]))
+
+
+# grouping 
+# ex: {john:21, maru:20,alice:21,joe:20}
+# o/p: {20:[joe,maru],21:[john,alice]}
+
+pupils = {"johnny":21,"sethu":22,"dani":22,"yuvaraj":21}
+team = {}
+
+for i in pupils:
+    if pupils[i] in team:
+        team[pupils[i]].append(i)
     else:
-        freq[i] = 1
-print(freq)
-print(sorted(freq,key=lambda x:freq[x]))
+        team[pupils[i]] = [i]
+
+print(team)
