3a55aa83dfaccaaf2004ec159c1f843f6f5ea5ea
Prawinkumarjs <prawink554@gmail.com>
2026-07-02 17:39:21 +0530

    sort string wrt frequency

--- dict.py
+++ dict.py
@@ -1,11 +1,24 @@
-# count frequency
+# # count frequency
 
-string = "aaabbbbcccccddddsss"
-fre ={}
+# string = "aaabbbbcccccddddsss"
+# fre ={}
 
-for i in string:
-    if i in fre:
-        fre[i] += 1
+# for i in string:
+#     if i in fre:
+#         fre[i] += 1
+#     else:
+#         fre[i] = 1
+# print(fre)
+
+# sort string wrt frequency
+
+word = "deiii vaaaddaaaa"
+freq = {}
+
+for i in word:
+    if i in freq:
+        freq[i]+=1
     else:
-        fre[i] = 1
-print(fre)
+        freq[i] = 1
+print(freq)
+print(sorted(freq,key=lambda x:freq[x]))
